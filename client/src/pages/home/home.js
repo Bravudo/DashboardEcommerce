904a50dc0cc4c8abdb3f025bf8b2be47390d5f72
Bravudo <gcursino.souza@gmail.com>
2025-08-08 17:05:50 -0300

    Pagina de test funcionando

---
+++ client/src/pages/home/home.js
@@ -0,0 +1,13 @@
+import styles from './home.module.css'
+
+function Home(){
+    return(
+        <div>
+            <div className={styles.test}>
+            <h1>aihgihashgi</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
